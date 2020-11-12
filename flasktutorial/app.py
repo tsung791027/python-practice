816c4a1616b42338d5b1ab3b49d81a8879bf25e6
jasper <tsung791027@gmail.com>
2020-11-12 23:07:57 +0800

    SQLAlchemy  model 定義

--- flasktutorial/app.py
+++ flasktutorial/app.py
@@ -3,11 +3,20 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
 bootstarp = Bootstrap(app)
 
 #用GET和POST request方法處理表單
@@ -39,8 +48,21 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('submit')
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
 
+def __repr__(self):
+    return '<Role %r>' % self.name
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+def __repr__(self):
+    return '<User %r>' % self.username
 
 if __name__ == '__main__':
     app.run(debug=True)
