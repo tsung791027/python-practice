84049b95e4bb640e1bd0da55a6261689e90845c9
jasper <tsung791027@gmail.com>
2020-11-12 23:23:07 +0800

    定義模組關係

--- flasktutorial/app.py
+++ flasktutorial/app.py
@@ -47,19 +47,23 @@ def internal_server_error(e):
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('submit')
-
+#定義db.model model
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), unique = True)
+    #model relationship
+    user = db.relationship('User', backref='role')
 
 def __repr__(self):
     return '<Role %r>' % self.name
-
+#定義db.model user
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    # model relationship
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 def __repr__(self):
     return '<User %r>' % self.username
