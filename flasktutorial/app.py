70717121368a9de87a07df7fb3c694a568cb3297
jasper <tsung791027@gmail.com>
2020-11-15 19:37:22 +0800

    flask mail pass

--- flasktutorial/app.py
+++ flasktutorial/app.py
@@ -16,11 +16,11 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAIL_SERVER'] ='smtp.googlemail.com'
+app.config['MAIL_SERVER'] ='smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = 'jasperli0407@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Tsung791027'
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
 app.config['FLASKY_SENDER'] = 'Jasper Li <jasperli0407@gmail.com>'
 
@@ -77,12 +77,12 @@ def internal_server_error(e):
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
-@app.route("/message")
-def send_email(to, subject, template, **kwargs):
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
-                  sender=app.config['FLASKY_SENDER'], recipients=[to])
-    msg.body = render_template(template + '.txt', **kwargs)
-    msg.html = render_template(template + '.html', **kwargs)
+@app.route('/message')
+def send_email():
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] ,
+                  sender=app.config['FLASKY_SENDER'], recipients=['jasperli0407@gmail.com'])
+    #msg.body = render_template(template + '.txt', **kwargs)
+    #msg.html = render_template(template + '.html', **kwargs)
     mail.send(msg)
     return 'You Send Mail by Flask-Mail Success!!'
 
