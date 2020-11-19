5a0fcb68d92c90f930ec5af4446c367f47cd5f99
myMAC <tsung791027@gmail.com>
2020-11-19 23:37:26 +0800

    將結構轉成大型app結構...待解決

--- flasktutorial/config.py
+++ flasktutorial/config.py
@@ -4,12 +4,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT','587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS','true').lower() in \
         ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = 'jasperli0407@gmail.com'
+    MAIL_PASSWORD = 'Tsung791027'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Jasper Li<jasperli0407@gmail.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
@@ -20,3 +20,22 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URL = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URL = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
